3b4394ba24d756ea94e7f9db6fc87565b5d154f4
Faiz Khalid <fz.khalid123@gmail.com>
2026-01-01 20:39:24 +0530

    Remove default tests and add money tests

--- 16-Testing/moneyTest.js
+++ 16-Testing/moneyTest.js
@@ -1,10 +1,10 @@
-import {formatCurrency} from "./scripts/money.js";
+import {formatCurrency} from './scripts/money.js';
 
-console.log('test suite: formatCurrency');
+console.log('Test suite: formatCurrency');
 
-console.log('It converts cents to dollars');
+console.log('It converts cents into dollars')
 
-if (formatCurrency(13270) === '132.70') {
+if (formatCurrency(2095) === '20.95') {
     console.log('passed');
 } else {
     console.log('failed');
@@ -18,7 +18,7 @@ if (formatCurrency(0) === '0.00') {
     console.log('failed');
 }
 
-console.log('It round up to the nearest cent');
+console.log('It rounds up to the nearest cent')
 
 if (formatCurrency(2000.5) === '20.01') {
     console.log('passed');
@@ -26,7 +26,7 @@ if (formatCurrency(2000.5) === '20.01') {
     console.log('failed');
 }
 
-console.log('It converts cents to dollars and round down');
+console.log('It converts cents into dollars and rounds down')
 
 if (formatCurrency(2000.4) === '20.00') {
     console.log('passed');
@@ -34,7 +34,4 @@ if (formatCurrency(2000.4) === '20.00') {
     console.log('failed');
 }
 
-
-console.log('\n' + 'test suite: calculateTax');
-
-
+console.log('\n' + 'Test suite: calculateTax');
