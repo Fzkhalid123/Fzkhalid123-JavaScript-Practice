ab6ef63e79c889b7e6c9ff27a0068e499148cc54
Faiz Khalid <fz.khalid123@gmail.com>
2026-01-14 15:21:12 +0530

    Lecture - 17 - Object Oriented Programming - Classes and Inheritance (#9)
    * AdvancedFunctionPart1
    
    * AdvancedFunction2
    
    * Build Amazon Project
    
    * update
    
    * update
    
    * Deleted old code and updated cart script
    
    * Save date and generate HTML for delivery options
    
    * Make delivery options interactive
    
    * Regenerate HTML when updating delivery option
    
    * Move orderSummary into its own file
    
    * Remove orders and tracking pages and styles
    
    * Save data and generate HTML for payment summary
    
    * Make payment summary interactive
    
    * remove spaces
    
    * Updated 15 External Libraries
    
    * create automated tests for formatCurrency
    
    * Add jasmine testing framework
    
    * Remove default tests and add money tests
    
    * create automated tests for addToCart
    
    * Create automated tests for orderSummary
    
    * Rename tests-jasmine folder to tests
    
    * Added Test
    
    * Create oop version of cart
    
    * create class version of cart
    
    * Make localStorageKey and loadFromStorage private
    
    * Convert products into classes
    
    * Use inheritance and polymorphism to add size chart
    
    * Learned about Object Oriented Programming

--- 13- Amazon-Project/data/products.js
+++ 13- Amazon-Project/data/products.js
@@ -1,4 +1,86 @@
-const products = [
+import {formatCurrency} from '../scripts/utils/money.js';
+
+export function getProduct(productId) {
+  let matchingProduct;
+
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+
+  return matchingProduct;
+}
+
+export class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
+
+  getPrice() {
+    return `$${(this.priceCents / 100).toFixed(2)}`;
+  }
+
+  extraInfoHTML() {
+    return '';
+  };
+}
+
+export class Clothing extends Product {
+  sizeChartLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.sizeChartLink = productDetails.sizeChartLink;
+  }
+
+  extraInfoHTML() {
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">
+      Size chart
+    </a>
+    `;
+  }
+}
+
+export class Appliance extends Product {
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML() {
+    return `
+    <a href="${this.instructionsLink}" target="_blank">
+    Instructions 
+    </a>
+    <a href="${this.warrantyLink}" target="_blank">
+    Warranty
+    </a>
+    `;
+  }
+}
+
+export const products = [
+  
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
@@ -58,7 +140,10 @@ const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: 'appliance',
+    instructionsLink: "images/appliance-instructions.png",
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -101,8 +186,11 @@ const products = [
     keywords: [
       "hoodies",
       "sweaters",
-      "apparel"
-    ]
+      "apparel",
+      "clothing"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
   },
   {
     id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
@@ -243,7 +331,10 @@ const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ],
+    type: 'appliance',
+    instructionsLink: 'images/appliance-instructions.png',
+    warrantyLink: 'images/appliance-warranty.png'
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -608,7 +699,10 @@ const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ],
+    type: 'appliance',
+    instructionsLink: 'images/appliance-instructions.png',
+    warrantyLink: 'images/appliance-warranty.png'
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -654,8 +748,11 @@ const products = [
       "sweaters",
       "hoodies",
       "apparel",
-      "mens"
-    ]
+      "mens",
+      "clothing"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
   }, 
   {
     id: "id-1",
@@ -699,7 +796,21 @@ const products = [
     keywords: [
       "styling",
       "summer",
-      "tshirt"
-    ]
+      "tshirt",
+      "clothing"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+
   }
-];
+].map((productDetails) => {
+  if(productDetails.type === 'clothing') {
+    return new Clothing(productDetails);
+  } else if (productDetails.type === 'appliance') {
+    return new Appliance(productDetails);
+  }
+
+  return new Product(productDetails);
+});
+console.log(products);
+
---
+++ 16-tests-jasmine/utils/moneyTest.js
@@ -0,0 +1,23 @@
+import { formatCurrency } from "../../15- external-libraries/scripts/utils/money.js";
+
+describe("test suite: formatCurrency", () => {
+  it("It converts a cent into dollars", () => {
+    expect(formatCurrency(2095)).toEqual("20.95");
+  });
+
+  it("It works with 0", () => {
+    expect(formatCurrency(0)).toEqual("0.00");
+  });
+
+  it("It rounds up to the nearest cent", () => {
+    expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+
+  it("It converts cents into dollars and rounds down", () => {
+    expect(formatCurrency(2000.4)).toEqual("20.00");
+  });
+
+  it("It ", () => {
+    expect(formatCurrency(2000.4)).toEqual("20.00");
+  });
+});
